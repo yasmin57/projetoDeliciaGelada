bf71b4d7115a3abd6f5ec384945def4ba780c8dd
yasmin57 <pereirayasmin57@gmail.com>
2019-09-30 19:21:36 -0300

    pagina sobre a empresa + produto do mês

---
+++ raiz/produto.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>
+            Produto Do Mês | Delicia Gelada
+        </title>
+       <link href="../css/style.css" type="text/css" rel="stylesheet">
+       <link href="../css/produto.css" type="text/css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+       <!-- CABEÇALHO -->
+        <?php require_once("header.php"); ?>
+       
+       <!-- CONTEÚDO      -->
+       <div id="produto_mes_main" class="back">
+          <div class="conteudo back center">
+             <div id="img_limao" class="back"></div>
+             <section class="fonte texto">
+                <h2> Suco de Limão </h2>
+                <p> Hoje em dia, o limão é queridinho na dieta e refeições de muitas pessoas. Deixou a fama de ser uma fruta que só possui acidez e o azedo, e atualmente, está presente na lista de frutas preferidas de muitas pessoas. Isso, porque possui diversos benefícios para saúde! </p>
+                <p>
+                Engana-se quem acha que o suco de limão é indicado apenas para os dias mais quentes, porque transmite frescor.
+                </p>
+                <p>
+                  Segundo dados do Sebrae, além do uso na culinária e preparo de bebidas, o limão é utilizado na produção de suco concentrado. A indústria de suco utiliza 40% a 50% do fruto, sendo o restante considerado resíduo industrial.
+                </p>
+                
+                <p>
+                  Escolha sempre uma marca de confiança, que não use conservantes, para obter o melhor do sabor e das propriedades das frutas. Conheça a Life Sucos!
+                </p>
+                 <div id="botoes">
+                     <div class="produto_mes_btn fonte float"> <a href="home.php">Compre</a> </div>
+                     <div class="produto_mes_btn fonte float"><a href="https://www.google.com.br" target="_blank">Saiba Mais</a></div>
+                 </div>
+             </section>
+          </div>
+       </div>
+       
+       <!-- RODAPÉ  -->
+       <?php require_once("footer.php"); ?>
+    </body>
+</html>
---
+++ raiz/sobre.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>
+            Sobre | Delicia Gelada
+        </title>
+       <link href="../css/style.css" type="text/css" rel="stylesheet">
+       <link href="../css/sobre.css" type="text/css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+       <!-- CABEÇALHO -->
+        <?php require_once("header.php"); ?>
+        
+        <!-- CONTEUDO -->
+        <div id="sobre">
+            <!--  Sobre nós   -->
+            <div id="sobre_nos" class="back_pink">
+               <div class="conteudo center back_pink">
+                    <table id="table_sobre_nos" class="center">
+                        <tr>
+                            <td id="titulo_sobre_nos">
+                                Sobre - nós
+                            </td>
+                            <td class="fonte textos">
+                                <p>
+                                    A Delícia Gelada é brasileira e nasceu
+                                    da vontade de oferecer um produto
+                                    diferente de tudo o que você já experimentou.
+                                </p>
+                                <p>
+                                    Foram anos trabalhando com parceiros
+                                    de tecnologia da Alemanha, EUA, França,
+                                    Holanda, Japão e Suécia para fazer
+                                    um suco de verdade, 100% natural
+                                    e que preserva ao máximo o sabor
+                                    de cada ingrediente, porque é produzido
+                                    a partir de frutas e vegetais frescos.
+                                </p>
+                                <p>
+                                    Hoje, brasileiros e pessoas do mundo todo
+                                    já aproveitam os benefícios do suco
+                                    da tampinha verde que você toma
+                                    aí na sua casa.
+                                </p>
+                                <p>
+                                    Queremos estar presentes no seu dia,
+                                    na sua casa e na sua vida
+                                    de um jeito natural,
+                                    assim como tudo o que fazemos.
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+               </div>
+            </div>
+            <!-- Titulo : Entenda a diferença  -->
+            <div id="entenda_diferenca" class="back_orange">
+                 <div class="conteudo center back_orange">
+                     <section id="titulo_entenda_diferenca" class="center">
+                         <h2> Entenda a diferença de Delícia gelada para outros produtos </h2>
+                     </section>
+                 </div>
+            </div>
+            <!-- O que fazemos? -->
+            <div id="oque_fazemos" class="back_goiaba">
+                 <div class="conteudo center back_goiaba">
+                     <table id="table_oque_fazemos" class="center">
+                        <tr>
+                            <td id="img_oque_fazemos" class="back">
+                            </td>
+                            <td class="fonte textos">
+                                <h2> O que fazemos?</h2>
+                                 <p>Acreditamos que saudável também deve ser sinônimo de gostoso. E vamos combinar: com tanto carinho, frutas gostosas, paixão e vegetais selecionados não tem como dar errado, não é mesmo?</p>
+                                <p>
+                                As bebidas da nossa família são a união da energia e dos benefícios das frutas e vegetais da natureza com o trabalho de especialistas, pesquisadores e detalhistas. Cuidamos de cada etapa com muito carinho e dedicação, investimos na produção própria e na transparência dos processos para buscar a sua plena satisfação.
+                                </p>
+                            </td>
+                        </tr>
+                     </table>
+                 </div>
+            </div>
+            <!-- Como fazemos? -->
+            <div id="como_fazemos" class="back_green">
+                 <div class="conteudo center back_green">
+                     <table id="table_como_fazemos" class="center">
+                         <tr id="linha_img_como_fazemos">
+                            <td id="img_como_fazemos"  class="back">
+                            </td>
+                        </tr>
+                        <tr id="linha_txt_como_fazemos">
+                            <td class="textos fonte center">
+                                <h2>Como Fazemos?</h2>
+                                 <p>Assim como todas as frutas, as nossas também vêm das fazendas. A diferença é que nossas fazendas são selecionadas. Todas seguem padrões internacionais de qualidade extremamente rigorosos. Assim, garantimos que as frutas e os vegetais que saem dos pomares e das hortas de todo o Brasil sejam preservados ao máximo até chegar à sua casa com tudo o que eles têm de melhor.</p>
+                            </td>
+                        </tr>
+                     </table>
+                 </div>
+            </div>
+            <!-- missão, visão e valores-->
+            <div id="valores" class="back_orange">
+                
+                <div class="conteudo center back_orange">
+                    <h2 class="fonte"> Quais são os nossos valores?</h2>
+                    <!-- missão -->
+                    <div class="sobre_valores float">
+                        <h1> Missão </h1>
+                        <div class="icones_valores center">
+                            <img alt="imagem" src="../imgs/icon_mission2.png">
+                        </div>
+                        <div class="fonte center textos_valores">
+                            <p>
+                                SER UMA EMPRESA DE EXCELÊNCIA NA PRODUÇÃO DE SUCOS E DERIVADOS, ATENDENDO TODOS OS PADRÕES DE QUALIDADE, COM CONSCIÊNCIA AMBIENTAL E RESPEITO À COMUNIDADE.
+                            </p>
+                        </div>
+                    </div>
+                    <!-- visão -->
+                    <div class="sobre_valores float">
+                        <h1> Visão </h1>
+                        <div class="center icones_valores">
+                            <img alt="imagem" src="../imgs/icon_vision.png">
+                        </div>
+                        <div class="fonte center textos_valores">
+                            <p>
+                                TORNAR-SE A MARCA DE SUCOS NATURAIS MAIS CONSUMIDA NO MERCADO NACIONAL, OFERECENDO PRODUTOS DE ALTA QUALIDADE E DE SABOR INCOMPARÁVEL.
+                            </p>
+                        </div>
+                    </div>
+                    <!-- valores -->
+                    <div class="sobre_valores float">
+                        <h1> Valores </h1>
+                        <div class="center icones_valores">
+                            <img alt="imagem" src="../imgs/icon_value1.png">
+                        </div>
+                        <div class="fonte center textos_valores">
+                            <p>
+                                Proporcionar momentos alegres e saudáveis para todos com INTEGRIDADE, TRANSPARÊNCIA, COMPROMETIMENTO, INOVAÇÃO E QUALIDADE.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+            
+        
+        <!-- RODAPÉ  -->
+       <?php require_once("footer.php"); ?>
+    </body>
+</html>
