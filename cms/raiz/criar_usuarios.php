c0c837360fce6e2b764d691e86b5d2cda487d083
yasmin57 <pereirayasmin57@gmail.com>
2019-10-23 16:55:34 -0300

    23/10 - CMS, criação de usuários

--- cms/raiz/criar_usuarios.php
+++ cms/raiz/criar_usuarios.php
@@ -16,36 +16,39 @@
                 <h1 class="txt_center">Administração de Usuários </h1>
                 
                 <form method="post" action="" name="frmusuarios" >
-                    <table id="frm_usuarios" class="center fonte">
+                    <table id="frm_usuarios" class="center fonte back_green_dark color_white">
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td  id="frm_usuarios_img"  class="back_green_light" rowspan="7">
+                                <div class="icon_user" id="div_usuarios_img" ></div>
+                            </td>
+                            <td class="frm_usuarios_name txt_center">
                                 <p> Nome: </p>
                             </td>
-                            <td class="back_pink_light"> <input name="txtnome" class="fonte" type="text" maxlength="100" required size="29"> </td>
+                            <td> <input name="txtnome" class="fonte" type="text" maxlength="100" required size="29"> </td>
                         </tr>
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td class="frm_usuarios_name txt_center">
                                 <p> Email: </p>
                             </td>
-                            <td class="back_pink_light"> <input name="txtemail" class="fonte" type="email" maxlength="1000" required size="29"> </td>
+                            <td> <input name="txtemail" class="fonte" type="email" maxlength="1000" required size="29"> </td>
                         </tr>
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td class="frm_usuarios_name  txt_center">
                                 <p> Telefone: </p>
                             </td>
-                            <td class="back_pink_light"> <input name="txttelefone" class="fonte" type="text" maxlength="15" required size="29"> </td>
+                            <td> <input name="txttelefone" class="fonte" type="text" maxlength="15" required size="29"> </td>
                         </tr>
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td class="frm_usuarios_name txt_center">
                                 <p> Registro: </p>
                             </td>
-                            <td class="back_pink_light"> <input name="txtregistro" class="fonte" type="text" maxlength="5" required size="29"> </td>
+                            <td> <input name="txtregistro" class="fonte" type="text" maxlength="5" required size="29"> </td>
                         </tr>
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td class="frm_usuarios_name txt_center">
                                 <p> Nível: </p>
                             </td>
-                            <td class="back_pink_light"> 
+                            <td> 
                                 <select name="slcnivel" id="frm_users_select" class="fonte">
                                     <option value="1"> Nível Administrador </option>
                                     <option value="2"> Nível Operador de Conteúdo </option>
@@ -54,23 +57,17 @@
                             </td>
                         </tr>
                         <tr>
-                            <td class="frm_usuarios_name back_black color_white txt_center">
+                            <td class="frm_usuarios_name txt_center">
                                 <p> Senha: </p>
                             </td>
-                            <td class="back_pink_light"> <input name="txtsenha" class="fonte" type="text" maxlength="1000" required size="29"> </td>
+                            <td> <input name="txtsenha" class="fonte" type="text" maxlength="1000" required size="29"> </td>
                         </tr>
                         <tr>
-                            <td class="back_black color_white txt_center" colspan="2">
+                            <td class="txt_center" colspan="2">
                                 <input class="botao back_green color_white fonte btn_users" type="submit" value="CRIAR" name="btncreateuser">
                             </td>
                         </tr>
                     </table>
-<!--
-                
-                    <div class="itens_frm_users">
-                        
-                    </div>
--->
                 </form>                
                 
             </section>
