388ad0afe1f6d0e12102e7770e8feacc51c24a55
yasmin57 <pereirayasmin57@gmail.com>
2019-10-23 11:16:12 -0300

    23/10

--- cms/raiz/criar_usuarios.php
+++ cms/raiz/criar_usuarios.php
@@ -16,31 +16,61 @@
                 <h1 class="txt_center">Administração de Usuários </h1>
                 
                 <form method="post" action="" name="frmusuarios" >
+                    <table id="frm_usuarios" class="center fonte">
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Nome: </p>
+                            </td>
+                            <td class="back_pink_light"> <input name="txtnome" class="fonte" type="text" maxlength="100" required size="29"> </td>
+                        </tr>
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Email: </p>
+                            </td>
+                            <td class="back_pink_light"> <input name="txtemail" class="fonte" type="email" maxlength="1000" required size="29"> </td>
+                        </tr>
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Telefone: </p>
+                            </td>
+                            <td class="back_pink_light"> <input name="txttelefone" class="fonte" type="text" maxlength="15" required size="29"> </td>
+                        </tr>
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Registro: </p>
+                            </td>
+                            <td class="back_pink_light"> <input name="txtregistro" class="fonte" type="text" maxlength="5" required size="29"> </td>
+                        </tr>
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Nível: </p>
+                            </td>
+                            <td class="back_pink_light"> 
+                                <select name="slcnivel" id="frm_users_select" class="fonte">
+                                    <option value="1"> Nível Administrador </option>
+                                    <option value="2"> Nível Operador de Conteúdo </option>
+                                    <option value="3"> Relacionamento com o Cliente </option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="frm_usuarios_name back_black color_white txt_center">
+                                <p> Senha: </p>
+                            </td>
+                            <td class="back_pink_light"> <input name="txtsenha" class="fonte" type="text" maxlength="1000" required size="29"> </td>
+                        </tr>
+                        <tr>
+                            <td class="back_black color_white txt_center" colspan="2">
+                                <input class="botao back_green color_white fonte btn_users" type="submit" value="CRIAR" name="btncreateuser">
+                            </td>
+                        </tr>
+                    </table>
+<!--
+                
                     <div class="itens_frm_users">
-                        <span>Nome: </span>
-                        <input name="txtnome" class="fonte" type="text" maxlength="100" required>
+                        
                     </div>
-                    <div class="itens_frm_users">
-                        <span>Email: </span>
-                        <input name="txtemail" class="fonte" type="email" maxlength="1000" required>
-                    </div>
-                    <div class="itens_frm_users">
-                        <span>Registro: </span>
-                        <input name="txtregistro" class="fonte" type="text" maxlength="100" required>
-                    </div>
-                    <div class="itens_frm_users"> <span>Nível: </span>
-                        <select name="slcnivel" class="fonte">
-                            <option value="1"> Nível Administrador </option>
-                            <option value="2"> Nível Operador de Conteúdo </option>
-                            <option value="3"> Relacionamento com o Cliente </option>
-                        </select>  
-                    </div>
-                    <div class="itens_frm_users">
-                        <span>Senha </span><input type="text" name="txtsenha" class="fonte" size="30" required>
-                    </div>
-                    <div class="itens_frm_users">
-                        <input id="btn_create_user" class="botao back_green color_white fonte" type="submit" value="criar" name="btnuser">
-                    </div>
+-->
                 </form>                
                 
             </section>
